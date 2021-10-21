efd8b0824b45a77403fd5625182f5b03615e5c1a
João Souza <56054550+jhon-kephler@users.noreply.github.com>
2021-10-21 11:34:58 -0300

    Add files via upload

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite um Numero: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            int num = 0;
+            var res = 0;
+
+            while (num <= limite)
+            {
+                               
+                if (num % 3 == 0)
+                {
+                    res += num;
+                }
+                if (num % 5 == 0)
+                {
+                    res += num;
+                }
+
+                num++;
+            }
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
